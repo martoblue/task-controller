ba44b02c82f902b9b6d3f46df1c453405bfa7749
Martorelli <luis.martorelli@bm-maritima.cl>
2024-01-31 15:25:41 -0300

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const taskRoutes = require('./routers/taskRouter');
+const taskRoutes = require('./routers/taskRoutes');
+const authRoutes = require('./routers/authRoutes');
 
 const app = express();
 const port = process.env.PORT ?? 3030;
@@ -14,6 +15,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(() => console.error('No se pudo conectar a MongoDB'));
+
+app.use('/api/users', authRoutes);
 
 // Registro de rutas
 app.use('/api/tasks', taskRoutes);
